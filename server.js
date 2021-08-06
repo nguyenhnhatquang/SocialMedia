aed47327022b53c498792cff5370494ec20e8642
ytuzu <nguyenhnhatquang@gmail.com>
2021-08-06 20:03:13 +0700

    config heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const SocketServer = require("./utils/SocketServer");
+const path = require("path");
 
 const corsOptions = {
   Credential: "true",
@@ -36,16 +37,12 @@ app.use("/api", require("./routes/postRouter.js"));
 // Connection Database
 require("./utils/ConnectDB");
 
-// Accessing the path module
-const path = require("path");
-
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Start server
 const PORT = process.env.PORT || 5000;
